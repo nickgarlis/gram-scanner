8858b593a4270311e65090e9050d2683271c0f70
nickgarlis <nickgarlis@gmail.com>
2018-04-21 18:34:22 +0300

    Release v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='gram-scanner',
-    version='0.1.0',
+    version='0.1.1',
     description="A command line interface of Ionian University's Gram-Web",
     author='Nick Garlis',
     author_email='nickgarlis@gmail.com',
