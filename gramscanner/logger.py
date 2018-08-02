ac20711af985ea38acfd5f339114d0918c27c0d9
nickgarlis <nickgarlis@gmail.com>
2018-08-02 18:56:07 +0300

    :wrench: print_grades

--- gramscanner/logger.py
+++ gramscanner/logger.py
@@ -11,13 +11,18 @@ class Logger:
 
   def print_grades(self, data):
     for course in data['courses']:
+      title = course['title']
+      grade = course['grade']
+
       if 'Consolidation' in course['title']:
         color = Fore.WHITE + Style.DIM
-      elif '-' in course['grade']:
+      elif '-' in grade:
         color = Fore.YELLOW
+      elif int(grade)<5:
+        color = Fore.RED
       else:
         color = Fore.GREEN
 
-      print(color + course['title'] + ' --> ' + Fore.WHITE + Style.BRIGHT + course['grade'] + Style.RESET_ALL, flush=True)
+      print(color + title + ' --> ' + Fore.WHITE + Style.BRIGHT + grade + Style.RESET_ALL, flush=True)
 
 Logger = Logger()
